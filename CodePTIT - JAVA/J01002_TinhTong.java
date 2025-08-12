d9ba82101ff2ac740eef2b54143de7ae08f7ab6d
nhhoang14 <nhhoangne@gmail.com>
2025-08-12 08:44:07 +0700

    update file

---
+++ CodePTIT - JAVA/J01002_TinhTong.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class J01002_TinhTong {
+    public static Scanner ip = new Scanner(System.in);
+
+    public static void testcase() {
+        long n = ip.nextLong();
+        long res = (n + 1) * n / 2;
+        System.out.println(res);
+    }
+
+    public static void main(String[] args) {
+        int tc = ip.nextInt();
+        while (tc-- >0){
+            testcase();
+        }
+    }
+}
