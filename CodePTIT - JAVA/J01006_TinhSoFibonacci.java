7605a5b1426d772da081bd5be1f722daf8dba328
nhhoang.14 <nhhoangne@gmail.com>
2025-08-26 18:39:14 +0700

    Add files via upload

---
+++ CodePTIT - JAVA/J01006_TinhSoFibonacci.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class J01006_TinhSoFibonacci {
+    public static void main(String[] args) {
+        Scanner ip = new Scanner(System.in);
+        int tc = ip.nextInt();
+        while(tc-- >0){
+            int n = ip.nextInt();
+            long f1 = 1, f2 = 1, f3 = 0;
+            if(n == 1 || n == 2) {
+                System.out.println(1);
+                continue;
+            }
+            for (int i = 3; i <= n; i++) {
+                f3 = f1 + f2;
+                f1 = f2;
+                f2 = f3;
+            }
+            System.out.println(f3);
+        }
+    }
+}
---
+++ CodePTIT - JAVA/J01007_KtraSoFibonacci.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class J01007_KtraSoFibonacci {
+    public static void main(String[] args) {
+        Scanner ip = new Scanner(System.in);
+        int tc = ip.nextInt();
+        while(tc-- >0){
+            long n = ip.nextLong();
+            long f1 = 1, f2 = 1, f3 = 0;
+            if(n == 0 || n == 1) {
+                System.out.println("YES");
+                continue;
+            }
+            while(f3 < n){
+                f3 = f1 + f2;
+                f1 = f2;
+                f2 = f3;
+            }
+            if(f3 == n)
+                System.out.println("YES");
+            else 
+                System.out.println("NO");                
+        }
+    }
+}
